9f3da01cfdc771630d546ff12e3c72da99fef916
Sergey Lobov <loboff09@gmail.com>
2023-02-05 12:47:10 +0300

    1.0

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
+from dataclasses import dataclass
+from typing import Optional, Union
 
-from flask import Flask
+from flask import Flask, request, render_template
+
+from utils import data_processing
 
 app = Flask(__name__)
 
@@ -8,9 +12,39 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-@app.post("/perform_query")
+@dataclass
+class Data:
+    file_name: str
+    cmd1: str
+    value1: Optional[str, int]
+    cmd2: str
+    value2: Optional[str, int]
+
+
+@app.post("/perform_query/", method=["POST"])
 def perform_query():
-    # нужно взять код из предыдущего ДЗ
+    try:
+        data: Data = request.json
+    except Exception as e:
+        return f'no data {e}'
+
+    try:
+        file_path = os.path.join(DATA_DIR, data.file_name)
+    except TypeError:
+        return f'Не передан аргумент "file_name"', 400
+
+    try:
+        cmd1 = data.cmd1
+        cmd2 = data.cmd2
+        val1 = data.value1
+        val2 = data.value2
+    except FileNotFoundError:
+        return f'File {data.file_name} does not exist', 400
+
+    with open(file_path, 'r', encoding='UTF-8') as file:
+        data_file = file.read().rstrip().split('\n')
+        first_response = data_processing(command=cmd1, value=val1, data=data_file)
+        second_response = data_processing(command=cmd2, value=val2, data=first_response)
+
     # добавить команду regex
     # добавить типизацию в проект, чтобы проходила утилиту mypy app.py
-    return app.response_class('', content_type="text/plain")
---
+++ utils.py
@@ -0,0 +1,21 @@
+def data_processing(command, value, data):
+    if command == "filter":
+        return [string for string in data if value in string]
+    elif command == "map":
+        return [string.split(' ')[int(value) - 1] for string in data]
+    elif command == "unique":
+        return list(set(data))
+    elif command == "sort":
+        if value == "desc":
+            return sorted(data, reverse=True)
+        else:
+            return sorted(data, reverse=False)
+    elif command == "limit":
+        return data[:int(value)]
+    elif command == 'regex':
+        return [string for string in data if re.compile(value).search(string)]
+
+
+
+
+
