8c7eea3f12591f828ed19d206795e5a6a513948d
Sergey Lobov <loboff09@gmail.com>
2023-02-07 00:00:08 +0300

    1.2 - app (33)

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 
 from dataclasses import dataclass
-from typing import Any, Optional, Type
+from typing import  List
 
 import marshmallow_dataclass
 
 from flask import Flask, request, render_template
-from marshmallow import Schema
+
 
 from utils import data_processing
 
@@ -14,28 +14,25 @@ app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
+CMD = ('filter', 'map', 'unique', 'sort', 'limit', 'regex')
 
 
 @dataclass
 class Data:
     file_name: str
     cmd1: str
-    value1: Optional[str]
+    value1: str
     cmd2: str
-    value2: Optional[str]
+    value2: str
 
 
 @app.post("/perform_query/")
 def perform_query() -> tuple[str, int]:
     try:
-        response: Optional[Any] = request.json
-        DataSchema: Type[Schema] = marshmallow_dataclass.class_schema(Data)
-        data: Data = DataSchema().load(response)
-
+        data_schema = marshmallow_dataclass.class_schema(Data)
+        data: Data = data_schema().load(data=request.json)
     except Exception:
-        return f'no data file', 400
-
-    file_path = os.path.join(DATA_DIR, data.file_name)
+        return f'no data', 400
 
     try:
         command_1 = data.cmd1
@@ -43,11 +40,15 @@ def perform_query() -> tuple[str, int]:
         value_1 = data.value1
         value_2 = data.value2
     except TypeError:
+        return 'No command', 400
+
+    if command_1 and command_2 not in CMD:
         return f'Unsupprted types', 400
 
     try:
+        file_path = os.path.join(DATA_DIR, data.file_name)
         with open(file_path, 'r', encoding='UTF-8') as file:
-            data_file = file.read().rstrip().split('\n')
+            data_file: List[str] = file.read().rstrip().split('\n')
             first_response = data_processing(command=command_1, value=value_1, data=data_file)
             second_response = data_processing(command=command_2, value=value_2, data=first_response)
         return render_template('response.html', data_file=second_response), 200
--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import re
-from typing import List, Optional
+from typing import List, Union, Sequence
 
 
-def data_processing(command: str, value: Optional[str], data: List[str]) -> List[str]:
+def data_processing(command: str, value: str, data: Sequence[str]) -> Sequence[str] :
     if command == "filter":
         return [string for string in data if value in string]
     elif command == "map":
@@ -16,5 +16,6 @@ def data_processing(command: str, value: Optional[str], data: List[str]) -> List
             return sorted(data, reverse=False)
     elif command == "limit":
         return data[:int(value)]
-    elif command == 'regex':
+    else:
         return [string for string in data if re.compile(value).search(string)]
+
